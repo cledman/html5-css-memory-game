3fd40eea3312f66790c3b9992f6e644b5341d95d
Cledson Lopes <cledman@gmail.com>
2020-09-25 17:44:44 -0300

    Sound added. Readme created.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Memory Game
+
+This project is part from the course "Creating a Memory game style with HTML5 and CSS3" from [Digital Innovation One](https://web.digitalinnovation.one/) using Javascript.
+
+<p align="center">
+    <img src=".github/preview.gif"/>
+</p>
+
+---
+
+# Guide
+
+- [Features](#features)
+- [Installation](#installation)
+- [Using](#using)
+- [Technologies](#technologies)
+- [Future Implementations](#future-implementations)
+
+# Features
+
+- Users can click flip cards.
+- Users can restart gane.
+
+# Installation
+
+`git clone https://github.com/cledman/html5-css-memory-game.git`
+
+# Using
+
+`Run the index.html file`
+
+# Technologies
+
+- [HTML](https://www.w3schools.com/html/) to write the page,
+- [CSS](https://www.w3schools.com/css/) to handle the styles, layout and rendering,
+
+# Future Implementations
+
+- The game layout could more responsible
+- We could have some music.
+- We could have score.
+
+Thanks to:
+
+- [Gabriela Pinheiro](https://www.linkedin.com/in/gabrielapinheiro129/)
+
+##
--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let totalFlips = 0;
 
+let soundFlip = document.getElementById("soundFlip");
+let soundVictory = document.getElementById("soundVictory");
+let soundLose = document.getElementById("soundLose");
+let soundFinal = document.getElementById("soundFinal");
+let button = document.getElementById("reestart");
 function flipCard() {
+  soundFlip.play();
   if (lockBoard) return;
   if (this === firstCard) return; /*to prevent double click on the same card */
 
@@ -20,7 +27,15 @@ function flipCard() {
 }
 
 function checkForMatch() {
+  console.log(totalFlips);
   if (firstCard.dataset.card === secondCard.dataset.card) {
+    totalFlips++;
+    if (totalFlips < 6) {
+      soundVictory.play();
+    } else {
+      button.classList.add("visible");
+      soundFinal.play();
+    }
     disableCards();
     return;
   }
@@ -35,6 +50,7 @@ function disableCards() {
 }
 
 function unflipCards() {
+  soundLose.play();
   lockBoard = true;
   setTimeout(() => {
     firstCard.classList.remove("flip");
@@ -54,6 +70,14 @@ function resetBoard() {
     card.style.order = ramdomPosition;
   });
 })();
+
+function reestart() {
+  location.reload();
+}
+
+function final() {
+  soundFinal.play();
+}
 
 cards.forEach((card) => {
   card.addEventListener("click", flipCard);
